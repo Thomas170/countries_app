6539982615fe21b2aa3ef3ec27e326529f336491
Thomas <thomas.cacelles@epita.fr>
2023-10-12 19:58:35 +0200

    init the README

--- backend/src/services/CountryService.js
+++ backend/src/services/CountryService.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
 const {CountryDetailsDto} = require("../models/CountryDetailsDto");
 
+const restCountriesApiUrl = process.env.REST_COUNTRIES_API_URL || 'https://restcountries.com/v3.1/name/';
+
 async function getCountryDetails(name) {
     try {
-        const response = await axios.get(`https://restcountries.com/v3.1/name/${name}`);
+        const response = await axios.get(`${restCountriesApiUrl}${name}`);
         if (response.data.length > 0) {
             return new CountryDetailsDto(response.data[0]);
         } else {
