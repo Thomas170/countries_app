08a8ccdc409393ea511b02e9c715b769161b3a5c
Thomas <thomas.cacelles@epita.fr>
2023-10-14 23:56:15 +0200

    fix env variable

--- frontend/src/services/CountryService.js
+++ frontend/src/services/CountryService.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const backendApiUrl = process.env.BACKEND_API_URL || 'http://localhost:8000/api/countries/';
+const backendApiUrl = process.env.BACKEND_API_URL || 'http://localhost:8000';
 
 export async function getCountryDetails(name) {
     try {
-        const response = await axios.get(`${backendApiUrl}${name}`);
+        const response = await axios.get(`${backendApiUrl}/api/countries/${name}`);
         return response.data;
     } catch (error) {
         throw error;
