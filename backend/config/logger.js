ba69900eb399df8be6ad5539eb0158b347fbbd9a
Thomas <thomas.cacelles@epita.fr>
2023-10-18 14:16:49 +0200

    remove logs folder

--- backend/config/logger.js
+++ backend/config/logger.js
@@ -4,8 +4,6 @@ const path = require('path');
 const logConfiguration = {
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'app_logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'app_logs/combined.log' })
     ],
     format: winston.format.combine(
         winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
