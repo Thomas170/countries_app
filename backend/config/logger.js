2ede89e7404f0aa11a4b1bb03c54c069653bec29
Thomas <thomas.cacelles@epita.fr>
2023-10-18 14:13:31 +0200

    create app_logs folder

--- backend/config/logger.js
+++ backend/config/logger.js
@@ -4,8 +4,8 @@ const path = require('path');
 const logConfiguration = {
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'logs/combined.log' })
+        new winston.transports.File({ filename: 'app_logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'app_logs/combined.log' })
     ],
     format: winston.format.combine(
         winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
